79deefb058b98c94c2d7541fe288475fc970c6ac
HungNgo03 <hungnnhe173202@fpt.edu.vn>
2025-03-08 15:38:37 +0700

    Initial commit

---
+++ app/src/main/java/com/example/final_project_prm392/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.example.final_project_prm392;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.final_project_prm392.databinding.ActivitySplashBinding;
+
+public class SplashActivity extends AppCompatActivity {
+    private ActivitySplashBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.startBtn.setOnClickListener(v -> startActivity(new Intent(SplashActivity.this, MainActivity.class)));
+    }
+}
